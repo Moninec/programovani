6095a98e0e4e400515f56144945d8b511668c472
Moninec <sittova121@trebesin.cz>
2024-10-09 10:47:15 +0200

    9.10.

--- python/ukazky/flask/flask_app.py
+++ python/ukazky/flask/flask_app.py
@@ -15,11 +15,19 @@ def form():
     name = request.args.get("name", default="________")
     input_class = request.args.get("class", default="________")
     message = request.args.get("message", default="________")
-    return  redirect(url_for("result", name=name, form_class=input_class, message=message))
+
+    if name != "________" and input_class != "________" and message != "________":
+        return  redirect(url_for("result", name=name, form_class=input_class, message=message))
+    
     return render_template("form.html")
 
 @app.route("/result")
 def result():
+    name = request.args.get("name", default="________")
+    input_class = request.args.get("form_class", default="________")
+    message = request.args.get("message", default="________")
+    return  render_template("result.html", name=name, form_class=input_class, message=message)
+    
 
 
 if __name__ == "__main__":
